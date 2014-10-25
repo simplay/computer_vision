14279d4681b6cea998dceda8b5ea88351018f76e
simplay <michael.single@students.unibe.ch>
2014-10-25 22:21:41 +0200

    Finding mean radius sphere mask

--- 1/code/fitChromeSphere.m
+++ 1/code/fitChromeSphere.m
@@ -24,7 +24,67 @@ function [L] = fitChromeSphere(chromeDir, nDir, chatty)
     im = imread(fname);
     imData(:,:,n) = im(:,:,1);           % red channel
   end
+  
+  % number of rows is height, num. of columns is width.
+  [imgHeight, imgWidth, imgCount] = size(imData);
+  
+  % sphere radius
+  r = maskSphereRadius(mask, imgHeight, imgWidth);
+  
 
-  % YOU NEED TO COMPLETE THIS FUNCTION
+  
   L = 1;
+end
+
+function r = maskSphereRadius(mask, imgHeight, imgWidth)
+    % find (min,max)x(height,width) indices which are equal to one on mask
+    % i.e. the boundary indices. compute their diameters and then, from
+    % those the corresponding radius.
+    
+    % make sure min will be updated 
+    minHeightIdx = imgHeight+ 1;
+    minWidthIdx = imgWidth + 1;
+    maxHeightIdx = -1;
+    maxWidthIdx = -1;
+    
+    % find '1' boundaries
+    for m=1:imgHeight,
+        for n=1:imgWidth,
+            if mask(m,n) == 1
+                minHeightIdx = relax(m, minHeightIdx, @isSmaller);
+                minWidthIdx = relax(n, minWidthIdx, @isSmaller);
+                maxHeightIdx = relax(m, maxHeightIdx, @isGrater);
+                maxWidthIdx = relax(n, maxWidthIdx, @isGrater);
+            end
+        end
+    end
+    % we have two diamenters: from minWidth to maxWidth
+    % and from minHeight to maxHeight. 
+    % We compute for these two diameters their radius - r = d/2 - 
+    % and compute the mean radius between these two radi.
+    diameters = [maxHeightIdx, maxWidthIdx]-[minHeightIdx, minWidthIdx];
+    r = mean(diameters/2);
+    
+    % internal anonymous functions for having a simpler handler handling.
+    function state = isSmaller(x,y)
+        % tests x < y
+        state = x < y;
+    end
+
+    function state = isGrater(x,y)
+        % tests x > y
+        state = x > y;
+    end
+end
+
+function newOptimum = relax(candidateOptimum, currentOptimum, relation)
+% if candidate optimum is better than current optimum according to a given
+% relation, then update the current optimum by the candidate optimum.
+% @param currentOptimum
+% @param candidateOptimimum
+% @param relation function handle tha returns a 1 for true & 0 for false.
+    newOptimum = currentOptimum;
+    if(relation(candidateOptimum, currentOptimum) == 1)
+        newOptimum = candidateOptimum;
+    end
 end
