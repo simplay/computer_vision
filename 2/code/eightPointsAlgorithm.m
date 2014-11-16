91234e699cae644571be1750a859c3db9db1ba5f
simplay <michael.single@students.unibe.ch>
2014-11-16 13:48:00 +0100

    Implemented 8-point algorithm in order to compute the fundamental matrix
    plus Added docs.

--- 2/code/eightPointsAlgorithm.m
+++ 2/code/eightPointsAlgorithm.m
@@ -1,4 +1,4 @@
-function F = eightPointsAlgorithm(x1, x2)
+function F = eightPointsAlgorithm(leftPositions, rightPositions)
 % function Fundamental_Matrix =Eight_Point_Algorithm(x1,x2)
 % Calculates the Fundamental matrix between two views from the normalized 8 point algorithm
 % Inputs: 
@@ -6,45 +6,86 @@ function F = eightPointsAlgorithm(x1, x2)
 %               x2      3xN     homogeneous coordinates of matched points in view 2
 % Outputs:
 %               F       3x3     Fundamental matrix
-
-    [x1_bar, T1] = normalizePosition(x1);
-    [x2_bar, T2] = normalizePosition(x2);
     
+    % Compute transformation matrices for left and right image feature positions.
+    leftTransform = getPositionNormalizationTransform(leftPositions);
+    rightTransform = getPositionNormalizationTransform(rightPositions);
     
+    % transform points from left and right image to their mean position
+    % having a distance of sqrt(2) to that position.
+    normalizedLeftPositions = leftTransform*leftPositions;
+    normalizedRightPositions = rightTransform*rightPositions;
+    
+    % define helper vectors in order to compute Y
+    u = normalizedLeftPositions(1,:);
+    v = normalizedLeftPositions(2,:);
+    u_ = normalizedRightPositions(1,:);
+    v_ = normalizedRightPositions(2,:);
+    
+    % Y is a homogenous matrix formed by all product combinations
+    % of the tranformed (x,y) coordinates of both image positions.
+    Y = [u_.*u; u_.*v; u_; v_.*u; v_.*v; v_; u; v];
+    Y = [Y; ones(1,length(leftPositions))];
+    
+    % compute the fundamental matrix that is formed by Y's U rotation
+    % matrix (resulting from Y's svd decomposition).
+    [U,~,~] = svd(Y);
+    F = reshape(U(:,end),3,3)';
+    
+    % ensure rank-2 constraint by setting the 3rd singular value to zero.
+    [U, S, V] = svd(F);
+    S(3,3) = 0;
+    
+    % reconstruct F (which is now rank two). Also normalze it by the image
+    % transformations.
+    F = U*S*V';
+    F = rightTransform'*F*leftTransform;
 end
 
 
-function [normalizedPositions, Transformation] = normalizePosition(positions)
-    pixelArea = 2;
+function normalizationTransformation = getPositionNormalizationTransform(positions)
+% Compute a Transformation Matrix such that when the given position inputs
+% are transformed by this matrix they are approximately centered around the
+% origin. Furthermore such points will have an average distance of sqrt(2)
+% to the origin. Note that the origin is determined as the mean position of
+% a given point cloud.
+% @param position a 3 x N Matrix, containing N homogenous 2d positions.
+% @return normalizationTransformation homohenous transform that transforms
+%         points from the input to the origin (mean position in
+%         pointcloud having an average distance of sqrt(2) to the origin).
+
+    
+    % get number of points
     N = length(positions);
     
     % since all positions are in homogenous coordinates
-    % select only the first two rows
+    % select only the first two rows - i.e. the (x,y) components of the points.
     xyPositions = positions(1:2,:);
     
-    % compute average x and y coordinate which is supposed to define the 
+    % compute mean x and y coordinate which is supposed to define the 
     % center position of the given point cloud.
-    avgPosition = (1/N)*sum(xyPositions, 2);
+    meanPosition = mean(xyPositions, 2);
     
-    % shift all positions towards the average position
-    centeredXY = xyPositions - repmat(avgPosition, 1, N);
+    % shift all positions towards the average position, i.e. shift them to
+    % the origin.
+    centeredXY = xyPositions - repmat(meanPosition, 1, N);
     
-    % compute distance from all position to the average 'center' position.
+    % compute the distance from all position to the mean 'center' position.
     dist2Center = sqrt(centeredXY(1, :).^2 + centeredXY(2, :).^2);
     
-    % compute the average distance to the average position among the
+    % compute the mean distance to the mean position among the
     % points in the point cloud.
-    avgDistance = (1/N)*sum(dist2Center);
+    meanDistance = mean(dist2Center);
     
-    % average distance to the points = sqrt(2)
-    s = sqrt(pixelArea)/avgDistance;
+    % ensure that the mean distance of the points to the origin is sqrt(2).
+    % for this we define the scale factor s.
+    s = sqrt(2)/meanDistance;
     
-    % scale all coordiantes by s
-    % and shift them by scaled center
-    Transformation = [s, 0, -s*avgPosition(1); 
-         0, s, -s*avgPosition(2);
+    % define a homogenous transformation matrix that scales that
+    % tranforms the input points to the origin (to their mean position)
+    % having a distance of sqrt(2) to this origin.
+    % scale all coordiantes by s and shift them by scaled center
+    normalizationTransformation = [s, 0, -s*meanPosition(1); 
+         0, s, -s*meanPosition(2);
          0, 0, 1];
-    
-    % [(s*xy - repmat(s*avgPosition, 1, N));ones(1,N)]
-    normalizedPositions = Transformation*positions;
 end
