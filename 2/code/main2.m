c2f70f496d97c4cbe18a468941d3c176fbccb909
simplay <michael.single@students.unibe.ch>
2014-11-17 01:10:47 +0100

    Implemented task 2

--- 2/code/main2.m
+++ 2/code/main2.m
@@ -54,73 +54,80 @@ tr = zeros(3,1);
 
 R90 = [0 -1 0; 1 0 0; 0 0 1];
 
-
 [U,S,V] = svd(E);
+
+%%
+
+% compute all 4 candidate rotations
 tmpR90 = R90;
 sign = 1;
 idx = 0;
 candidateRotation = [];
-%%
-
-% compute all 4 candidate rotations
 for k=1:4
-    tmpR = sign*U*tmpR90*V;
+    tmpR = sign*U*tmpR90*V';
     
+    % if determinate is greater zero.
+    % actually for any rotation matrix, the determinate is either 1 or -1.
     if det(tmpR) > 0
         idx = idx + 1;
         candidateRotation = [candidateRotation,tmpR];
     end
     
+    % sign swapping after first half of iterations
     if (k==2) sign = -sign; end
+    
+    % transpose matrix: every even iteration it is flipped initial version. 
     tmpR90 = tmpR90';
 end
 
 % reshape to a 3x3xN tensor condaining all N 3x3 candidate rotational transformations. 
 candidateRotation = reshape(shiftdim(candidateRotation,1)', 3, 3, idx);
-R = candidateRotation;
 
+% set candidate translations
 candidateTranslations = zeros(3,1,2);
-t_x = V*R90*S*V';
-t = [t_x(3,2); t_x(1,3); t_x(2,1)];
-t = t/norm(t);
 candidateTranslations(:,:,1) = U(:,end);
 candidateTranslations(:,:,2) = -U(:,end);
-t = candidateTranslations;
-% get best combination
-current = -Inf;
+
+% compute height values from rotation and translation transformation
+% combinations.
 f = 4;
-for k=1:2
-    for j=1:2
-        %R(:,:,k) = Rr;
-        %t(:,j) = Tr;
-        a = repmat(R(1,:,k),size(rightPoints,2),1) - repmat(rightPoints(1,:)',1,3).*repmat(R(3,:,k),size(rightPoints,2),1);
-        x3 = sum(a .* repmat(t(:,j), 1, size(rightPoints,2))',2) ./  sum(a .* leftPoints',2);
+zComponents = zeros(M,1, size(candidateTranslations,3)*size(candidateRotation,3));
+idx = 1;
+for k=1:size(candidateRotation,3)
+    for j=1:size(candidateTranslations,3)
+        %candidateRotation(:,:,k) = Rr;
+        %candidateTranslations(:,j) = Tr;
         
-        % update points if we find a rotation, translation combination
-        % where numbers in front of the camera is larger
-        numPositiveZ = length(find(x3 >= 0));
-        if numPositiveZ > current
-            current = numPositiveZ;
-            x1 = x3 ./ f .* leftPoints(1,:)';
-            x2 = x3 ./ f .* leftPoints(2,:)';
-            p = [x1,x2,x3]';
-        end
+        a = repmat(candidateRotation(1,:,k),size(rightPoints,2),1) ...
+            - repmat(rightPoints(1,:)',1,3).*repmat(candidateRotation(3,:,k),size(rightPoints,2),1);
+        z = sum(a .* repmat(candidateTranslations(:,j), 1, size(rightPoints,2))',2) ./  sum(a .* leftPoints',2);
+        zComponents(:,:,idx) = z;
+        idx = idx + 1;
     end
 end
 
+% take height values with most points in front of the camera,
+% this directly relates to the best rotation and best translation.
+bestZIdx = -1;
+bestNumPosZ = -1;
+for k=1:size(zComponents,3)
+    currentZ = zComponents(:,:,k);
+    
+    numPositiveZ = length(find(currentZ >= 0));
+    
+    % relax values
+    if(numPositiveZ > bestNumPosZ)
+        bestNumPosZ = numPositiveZ;
+        bestZIdx = k;
+    end
+end
 
 %plot 3D points
-x = p(1,:)';
-y = p(2,:)';
-z = p(3,:)';
+z = zComponents(:,:,bestZIdx);
+x = z ./ f .* leftPoints(1,:)';
+y = z ./ f .* leftPoints(2,:)';
 
 figure(2);
 scatter3(x,y,z, 'MarkerEdgeColor', [0,0,0], 'MarkerFaceColor', [0.5,0.5,0.5]);
 title('Reconstructed 3D-image');
 
-
-%candidateTranslations = size(
-
-% TODO: Reconstrct the 3D points (Question 3)
-
-% TODO: Visualize 3D points (Question 4)
